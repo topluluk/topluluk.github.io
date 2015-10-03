a9802d69cd9f14c48902cf22a1303a42b23043aa
Ömer KALA <kalaomer@hotmail.com>
2015-10-03 19:05:50 +0300

    new design and groups added

---
+++ app/app.module.js
@@ -0,0 +1,7 @@
+
+import groups from "./groups/groups.module";
+import angular from "angular";
+
+let app = angular.module("app", [groups]);
+
+export default app;
---
+++ app/groups/groups.module.js
@@ -0,0 +1,8 @@
+
+import controller from "./groups.controller";
+import angular from "angular";
+
+let groups = angular.module("groups", []);
+groups.controller("groups.controller", controller);
+
+export default groups.name;
