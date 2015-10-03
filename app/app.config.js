b1dd2a9fa67926869f9071b451e7b84a90fac9d4
Ömer KALA <kalaomer@hotmail.com>
2015-10-04 01:12:46 +0300

    hash prefix changed & links added

---
+++ app/app.config.js
@@ -0,0 +1,4 @@
+
+export default ["$locationProvider", function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+}];
--- app/app.module.js
+++ app/app.module.js
@@ -1,7 +1,9 @@
 
-import groups from "./groups/groups.module";
 import angular from "angular";
+import groups from "./groups/groups.module";
+import config from "./app.config";
 
 let app = angular.module("app", [groups]);
+app.config(config);
 
 export default app;
